2180485a4545c0dbb9e38913412854af5fbdfe58
Lstortoni <89618746+Lstortoni@users.noreply.github.com>
2024-08-26 22:16:51 -0300

    Cambios varios
    varios cambios iniciales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+
+  const configserv = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(configserv.get('PORT'));
 }
 bootstrap();
